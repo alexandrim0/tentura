88e378db565a7517fef6db7ce360db5a5a9d69bf
Postnikov Aleksander <alexandrim0@gmail.com>
2024-12-20 18:14:43 +0300

    - remove db support (for now)

--- packages/server/lib/consts.dart
+++ packages/server/lib/consts.dart
@@ -1,24 +1,15 @@
-const kDebugMode = bool.fromEnvironment('IS_DEBUG_MODE');
-
-const kPgHost = String.fromEnvironment(
-  'POSTGRES_USERNAME',
-  defaultValue: 'postgres',
+const kDebugMode = bool.fromEnvironment(
+  'IS_DEBUG_MODE',
 );
 
-const kPgDatabase = String.fromEnvironment(
-  'POSTGRES_DATABASE',
-  defaultValue: 'postgres',
+const kPublicKey = String.fromEnvironment(
+  'JWT_PUBLIC_PEM',
 );
 
-const kPgUsername = String.fromEnvironment(
-  'POSTGRES_USERNAME',
-  defaultValue: 'postgres',
+const kPrivateKey = String.fromEnvironment(
+  'JWT_PRIVATE_PEM',
 );
 
-const kPgPassword = String.fromEnvironment('POSTGRES_PASSWORD');
-
-const kPublicKey = String.fromEnvironment('JWT_PUBLIC_PEM');
-
-const kPrivateKey = String.fromEnvironment('JWT_PRIVATE_PEM');
-
-const kSentryDsn = String.fromEnvironment('SENTRY_DSN');
+const kSentryDsn = String.fromEnvironment(
+  'SENTRY_DSN',
+);
--- packages/server/lib/di/di.config.dart
+++ packages/server/lib/di/di.config.dart
@@ -8,11 +8,10 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:drift/drift.dart' as _i500;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:logger/logger.dart' as _i974;
-import 'package:tentura_server/data/database/database.dart' as _i635;
+import 'package:stormberry/stormberry.dart' as _i547;
 import 'package:tentura_server/di/modules.dart' as _i991;
 
 extension GetItInjectableX on _i174.GetIt {
@@ -28,11 +27,7 @@ extension GetItInjectableX on _i174.GetIt {
     );
     final registerModule = _$RegisterModule();
     gh.singleton<_i974.Logger>(() => registerModule.logger);
-    gh.singleton<_i500.QueryExecutor>(() => registerModule.database);
-    gh.singleton<_i635.Database>(
-      () => _i635.Database(gh<_i500.QueryExecutor>()),
-      dispose: (i) => i.dispose(),
-    );
+    gh.singleton<_i547.Database>(() => registerModule.database);
     return this;
   }
 }
