8ee3f3f569e9539ae70ada545ace64bc9e6caa86
V.G. Bulavintsev <golem.md@gmail.com>
2025-10-17 19:56:30 +0200

    Make invite code input accept URLs

--- packages/client/lib/ui/utils/string_input_validator.dart
+++ packages/client/lib/ui/utils/string_input_validator.dart
@@ -2,15 +2,19 @@ import 'package:tentura/ui/l10n/l10n.dart';
 
 import 'package:tentura/consts.dart';
 
+final _codePattern = RegExp(r'(I[a-f0-9]{12})');
+
 mixin StringInputValidator {
-  String? invitationCodeValidator(L10n l10n, String? code) {
-    if (code == null || code.length < kIdLength) {
+  String? invitationCodeValidator(L10n l10n, String? input) {
+    if (input == null || input.length < kIdLength) {
       return l10n.invitationCodeTooShort;
     }
-    if (code.length > kTitleMaxLength) {
-      return l10n.invitationCodeTooLong;
+    final match = _codePattern.firstMatch(input);
+    if (match == null) {
+      return l10n.invitationCodeWrongFormat;
     }
-    if (!code.startsWith('I')) {
+    final code = match.group(1)!;
+    if (code.length != kIdLength || !code.startsWith('I')) {
       return l10n.invitationCodeWrongFormat;
     }
     return null;
